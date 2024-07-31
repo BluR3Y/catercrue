4b079c7e338c795b45db5f3cbd542349afc9b7a8
Rey Flores <69604218+BluR3Y@users.noreply.github.com>
2024-07-31 04:34:36 -0400

    Started development on listing-service

--- backend/core-service/src/models/index.ts
+++ backend/core-service/src/models/index.ts
@@ -2,6 +2,8 @@ import { sequelize } from "../config/database";
 import Address from "./address.model";
 import Coordinator from "./coordinator.model";
 import Staff from "./staff.model";
+import StaffRole from "./staffRole.model";
+import StaffSeekingRole from "./staffSeekingRole.model";
 import User from "./user.model";
 
 // Syncronize all defined models to the DB
@@ -10,8 +12,10 @@ const db = {
     Sequelize: sequelize,
     User,
     Address,
+    Coordinator,
     Staff,
-    Coordinator
+    StaffRole,
+    StaffSeekingRole
 };
 
 export default db;
